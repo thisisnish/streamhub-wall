e079f49429ca632280a0106829961f2c4cc50132
Benjamin Goering <bengoering@gmail.com>
2014-06-12 13:32:32 -0700

    Update example to use new collection and remove sdk css from the page (hack)

--- example.js
+++ example.js
@@ -1,18 +1,26 @@
 define(function(require) {
     var Collection = require('streamhub-sdk/collection');
     var View = require('streamhub-wall');
+    var $ = require('streamhub-sdk/jquery');
 
     return function(el) {
         var collection = new Collection({
-            network: "labs-t402.fyre.co",
-            environment: "t402.livefyre.com",
-            siteId: "303827",
-            articleId: 'sh_col_51_1366914813'
+            network: "labs.fyre.co",
+            environment: "livefyre.com",
+            siteId: "315833",
+            articleId: 'livefyre-tweets'
         });
         var view = new View({el: el});
         
         collection.pipe(view);
-         
+        
+        // As streamhub-wall#3 now inserts its own css, when this is run
+        // on the app gallery we need to remove the sdk 2.7 css.
+        // this is a weird hack. Sorry.
+        $(function ($) {
+            $('link[href*="sdk.min.css"]').remove();
+        });
+
         return view;
     };
 });
