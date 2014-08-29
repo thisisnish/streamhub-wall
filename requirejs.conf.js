a250aefe5757d74c19df011b205abe25b2fc3082
Ryan Cheung <ryanc@livefyre.com>
2014-08-29 12:41:29 -0700

    Update livefyre-theme-styler dep to enable adaptive button styling

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -15,6 +15,7 @@ require.config({
     rework: 'lib/rework/rework',
     observer: 'lib/observer/src/observer',
     mout: 'lib/mout/src',
+    tinycolor: 'lib/tinycolor2/tinycolor',
     'livefyre-package-attribute': 'node_modules/livefyre-package-attribute/src/main',
     'node-uuid': 'lib/node-tiny-uuid/index'
   },
