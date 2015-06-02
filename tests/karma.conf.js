1c7be4f4282d48ffdf6dccb59806ab8d279e17dd
Benjamin Goering <bengoering@gmail.com>
2015-06-01 19:15:09 -0700

    Use karma and jasmine2 instead of old duct-tapey solution

---
+++ tests/karma.conf.js
@@ -0,0 +1,99 @@
+// Karma configuration
+// Generated on Thu Nov 21 2013 14:47:59 GMT-0800 (PST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '..',
+
+
+    // frameworks to use
+    frameworks: ['jasmine', 'cajon'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'requirejs.conf.js',
+      {pattern: 'package.json', included: false},
+      // shim for function.bind, needed for phantomjs
+      {pattern: 'tests/lib/function.bind.js', included: true},
+      {pattern: 'src/**/*.js', included: false},
+      {pattern: 'src/**/*.css', included: false},
+      {pattern: 'src/**/*.mustache', included: false},
+      {pattern: 'tests/jasmine.js', included: false},
+      {pattern: 'tests/spec/**/*.js', included: false},
+      {pattern: 'tests/mocks/**/*.js', included: false},
+      {pattern: 'lib/**/*.css', included: false},
+      {pattern: 'lib/**/*.txt', included: false},
+      {pattern: 'lib/**/*.js', included: false},
+      {pattern: 'lib/**/*.json', included: false},
+      {pattern: 'lib/**/*.mustache', included: false},
+      {pattern: 'dist/*', included: false},
+      {pattern: 'tests/**/*.json', included: false},
+      {pattern: 'node_modules/livefyre-package-attribute/**/*.js', included: false},
+      {pattern: 'node_modules/jasmine-jquery/**/*.js', included: false},
+      'tests/tests-main.js',
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_ERROR,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+    preprocessors: {
+      'src/**/*.js': 'coverage'
+    },
+
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
+
+    junitReporter: {
+      outputFile: 'tests.log',
+      suite: 'streamhub-sdk'
+    },
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,86 @@
-require(['jasmine', 'jasmine-html', 'jquery'], function (jasmine, jasmineHtml, $) {
-    var jasmineEnv = jasmine.getEnv();
-    jasmineEnv.updateInterval = 1000;
+require(['jasmine', 'jasmineRequire', 'jasmine-html', 'jquery'],
+function (jasmine, jasmineRequire, jasmineHtml, $) {
+    jasmineRequire.html(jasmine);
+
+    var env = jasmine.getEnv();
+    env.updateInterval = 1000;
+
+    window.jasmine = jasmine;
+    jasmine.addMatchers = function () {
+        return env.addMatchers.apply(env, arguments);
+    }
+
+    /**
+    * ## Runner Parameters
+    *
+    * More browser specific code - wrap the query string in an object and to allow for getting/setting parameters from the runner user interface.
+    */
+
+    var queryString = new jasmine.QueryString({
+        getWindowLocation: function() { return window.location; }
+    });
+
+    var catchingExceptions = queryString.getParam("catch");
+    env.catchExceptions(typeof catchingExceptions === "undefined" ? true : catchingExceptions);
+
+    var throwingExpectationFailures = queryString.getParam("throwFailures");
+    env.throwOnExpectationFailure(throwingExpectationFailures);
+
 
     // HTMLReporter
-	var htmlReporter = new jasmine.HtmlReporter();
-	jasmineEnv.addReporter(htmlReporter);
-	jasmineEnv.specFilter = function(spec) {
-		return htmlReporter.specFilter(spec);
-	};
+    var htmlReporter = new jasmine.HtmlReporter({
+        env: env,
+        onRaiseExceptionsClick: function() { queryString.navigateWithNewParam("catch", !env.catchingExceptions()); },
+        onThrowExpectationsClick: function() { queryString.navigateWithNewParam("throwFailures", !env.throwingExpectationFailures()); },
+        addToExistingQueryString: function(key, value) { return queryString.fullStringWithNewParam(key, value); },
+        getContainer: function() { return document.body; },
+        createElement: function() { return document.createElement.apply(document, arguments); },
+        createTextNode: function() { return document.createTextNode.apply(document, arguments); },
+        timer: new jasmine.Timer()
+    });
+	env.addReporter(htmlReporter);
 
-    // Copy jasmine globals
-    ['spyOn', 'waitsFor', 'waits', 'runs', 'expect'].forEach(function (key) {
-        window[key] = function () {
-            var spec = jasmine.getEnv().currentSpec;
-            return spec[key].apply(spec, arguments);
-        };
+    /**
+    * Filter which specs will be run by matching the start of the full name against the `spec` query param.
+    */
+    var specFilter = new jasmine.HtmlSpecFilter({
+        filterString: function() { return queryString.getParam("spec"); }
     });
-    ['beforeEach', 'afterEach', 'describe', 'it'].forEach(function (key) {
-        window[key] = jasmineEnv[key].bind(jasmineEnv);
-    });
-    require(['tests/spec/wall-view', 'tests/spec/wall-component', 'tests/spec/main', 'tests/spec/wall-header-view'], function(){
+
+    env.specFilter = function(spec) {
+        return specFilter.matches(spec.getFullName());
+    };
+
+    /**
+    * ## The Global Interface
+    *
+    * Build up the functions that will be exposed as the Jasmine public interface. A project can customize, rename or alias any of these functions as desired, provided the implementation remains unchanged.
+    */
+    var jasmineInterface = jasmineRequire.interface(jasmine, env);
+
+    /**
+    * Add all of the Jasmine global/public interface to the global scope, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
+    */
+    extend(window, jasmineInterface);
+
+    require([
+        'tests/spec/wall-view',
+        'tests/spec/wall-component',
+        'tests/spec/main',
+        'tests/spec/wall-header-view'
+    ], function(){
         $(function(){
-            jasmineEnv.execute();
+            htmlReporter.initialize()
+            env.execute();
         });
     });
+
+    /**
+     * Helper function for readability above.
+     */
+    function extend(destination, source) {
+        for (var property in source) destination[property] = source[property];
+        return destination;
+    }
+
 });
